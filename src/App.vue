6285a194065c1d0217d673693853ce4ee6b0d1a6
levkotman <leoktmn@yandex.ru>
2021-10-05 18:44:09 +0200

    started a new comp FindCar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="app">
-    <get-data />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <find-car />
   </div>
 </template>
 
 <script>
-import GetData from "./components/GetData.vue";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+
+import axios from "axios";
+import FindCar from "./components/FindCar.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-    GetData,
+    // HelloWorld,
+    FindCar,
+  },
+  data() {
+    return {
+      token:
+        "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOjUsInJlcyI6ImFnZ3JlZ2F0b3IiLCJpYXQiOjE2MzEwMTAwMjN9.YdfhveqPYc98LSxQAiB3Ern3wZFFWKsg5EejtC4q2bo",
+      availableAutos: [],
+      fetchedData: null,
+    };
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("http://localhost:8080/api_aggregator/vehicles/", {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Allow-Methods": ("GET", "POST", "OPTIONS"),
+            "Access-Control-Allow-Headers":
+              ("Origin", "Content - Type", "Accept"),
+            Authorization: `Bearer ${this.token}`,
+          },
+          responseType: "json",
+          proxy: {
+            protocol: "http",
+            host: "192.168.43.176",
+            port: 8080,
+          },
+        })
+        .then((res) => {
+          this.fetchedData = res.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    this.fetchData();
   },
 };
 </script>
---
+++ src/components/FindCar.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        Let's find a car
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
