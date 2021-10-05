4a519b9cb70732affbda0631fc3fb68a27c0a167
levkotman <leoktmn@yandex.ru>
2021-10-05 23:41:08 +0200

    prepare to deploy to gh pages

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh# abort on errors
+set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+git push -f git@github.com:leokotman/rent-vehicle.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,5 @@ module.exports = {
   devServer: {
     proxy: "https://prerelease.moqo.de/",
   },
+  publicPath: process.env.NODE_ENV === "production" ? "/rent-vehicle/" : "/",
 };
